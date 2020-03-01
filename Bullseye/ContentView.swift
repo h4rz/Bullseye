3135d30b6d933095dd6496209f0fbdfe64f47749
Harsh Rajgor <harsh@keubik.com>
2020-03-01 22:01:36 +0530

    Built UI for bullseye

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -11,22 +11,50 @@ import SwiftUI
 struct ContentView: View {
     
     @State var alertIsVisible: Bool = false
+    @State var sliderValue: Double = 50.0
     var body: some View {
         VStack {
-            VStack {
-                Text("Hello, Harsh!")
-                    .fontWeight(.semibold)
-                    .foregroundColor(Color.green)
-                Button(action: {
-                    self.alertIsVisible = true
-                }) {
-                    Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
-                        .padding(5.0)
-                }
-                .alert(isPresented: $alertIsVisible) { () -> Alert in
-                    return Alert(title: Text("Hello"), message: Text("First"),dismissButton: .default(Text("Awesome!")))
-                }
+            Spacer()
+            // Target Row
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                Text("100")
             }
+            Spacer()
+            
+            // Slider Row
+            HStack{
+                Text("1")
+                Slider(value: self.$sliderValue, in: 1...100)
+                Text("100")
+            }
+            Button(action: {
+                self.alertIsVisible = true
+            }) {
+                Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
+            }
+            .alert(isPresented: $alertIsVisible) { () -> Alert in
+                let roundedValue:Int = Int(self.sliderValue.rounded())
+                return Alert(title: Text("Result"), message: Text("The slider's value is \(roundedValue)."),dismissButton: .default(Text("Awesome!")))
+            }
+            Spacer()
+            
+            //Score Row
+            HStack{
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text("Score:")
+                Text("999999")
+                Spacer()
+                Text("Round:")
+                Text("999")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Info")
+                }
+            }.padding(.bottom,20)
         }
     }
 }
