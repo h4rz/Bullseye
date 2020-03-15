602c29684d102ba81628a59b38d078d6806e9c2d
Harsh Rajgor <harsh@keubik.com>
2020-03-15 21:53:10 +0530

    Completed Part 4 : More Swift Basics

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -13,6 +13,8 @@ struct ContentView: View {
     @State var alertIsVisible = false
     @State var sliderValue = 50.0
     @State var target = Int.random(in: 1...100)
+    @State var totalScore = 0
+    @State var round = 1
     var body: some View {
         VStack {
             Spacer()
@@ -35,22 +37,28 @@ struct ContentView: View {
                 Text(/*@START_MENU_TOKEN@*/"Hit Me!"/*@END_MENU_TOKEN@*/)
             }
             .alert(isPresented: $alertIsVisible) { () -> Alert in
-                return Alert(title: Text("Result"), message: Text("The slider's value is \(sliderValueRounded()).\n" +
-                    "You scored \(pointsForCurrentRound() ) points this round."),dismissButton: .default(Text("Awesome!")))
+                return Alert(title: Text(alertTitle()), message: Text("The slider's value is \(sliderValueRounded()).\n" +
+                    "You scored \(pointsForCurrentRound() ) points this round."),dismissButton: .default(Text("Awesome!")) {
+                        self.totalScore += self.pointsForCurrentRound()
+                        self.target = Int.random(in: 1...100)
+                        self.round += 1
+                    })
             }
             Spacer()
             
             //Score Row
             HStack{
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                Button(action: {
+                    self.resetGame()
+                }) {
                     Text("Start over")
                 }
                 Spacer()
                 Text("Score:")
-                Text("999999")
+                Text("\(totalScore)")
                 Spacer()
                 Text("Round:")
-                Text("999")
+                Text("\(round)")
                 Spacer()
                 Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
                     Text("Info")
@@ -59,12 +67,46 @@ struct ContentView: View {
         }
     }
     
+    func resetGame() {
+        totalScore = 0
+        round = 1
+        sliderValue = 50.0
+        target = Int.random(in: 1...100)
+    }
+    
     func sliderValueRounded() -> Int {
         Int(sliderValue.rounded())
     }
     
+    func differenceTargetNSliderValue() -> Int {
+        abs(target - sliderValueRounded())
+    }
+    
     func pointsForCurrentRound() -> Int {
-        100 - abs(target - sliderValueRounded())
+        let maximumScore = 100
+        let difference = differenceTargetNSliderValue()
+        var bonus = 0
+        if difference == 0 {
+            bonus = 100
+        } else if difference == 1 {
+            bonus = 50
+        }
+        return maximumScore - difference + bonus
+    }
+    
+    func alertTitle() -> String {
+        let difference = differenceTargetNSliderValue()
+        var title:String
+        if difference == 0 {
+            title = "Perfect!"
+        } else if difference < 5 {
+            title = "You almost had it!"
+        } else if difference <= 10 {
+            title = "Not bad."
+        } else {
+            title = "Are you even trying?"
+        }
+        return title
     }
 }
 
